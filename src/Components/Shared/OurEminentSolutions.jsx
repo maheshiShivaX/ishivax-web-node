563afe3d6b47970caf293f37f3849abda10376f4
Vikas <vikas@ishivax.com>
2026-04-21 12:07:03 +0530

    position remove kiya hai

--- src/Components/Shared/OurEminentSolutions.jsx
+++ src/Components/Shared/OurEminentSolutions.jsx
@@ -146,7 +146,7 @@ const OurEminentSolutions = () => {
 
                         {data?.length > 0 && data?.map((item, i) => (
                             <div className="col-lg-4 col-md-6 col-sm-6 col-xs-12 hover_inAll mb-lg-auto mb-4" key={i}>
-                                <Link to="/service-mobile-app" style={{ position: "relative" }}>
+                                <Link to="/service-mobile-app" >
                                     <div className="frame-7-XAX">
                                         <p className="mobile-app-development-2sy">{item?.title}</p>
                                         <div>
