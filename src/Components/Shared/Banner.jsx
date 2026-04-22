e24b02dbd854f26b6b738e15549fdaabaf2b138f
maheshiShivaX <mahesh@ishivax.com>
2026-04-22 17:15:28 +0530

    banner click scroll down on iphone

--- src/Components/Shared/Banner.jsx
+++ src/Components/Shared/Banner.jsx
@@ -20,7 +20,7 @@ const Banner = () => {
 
 
     // ✅ Device detection
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    const isMobile = /iPhone|iPad|iPod|Android|Linux/i.test(navigator.userAgent);
 
     const stopScroll = useCallback(() => {
         isScrollingRef.current = false;
@@ -54,7 +54,7 @@ const Banner = () => {
 
     const scrollToBottomSlow = () => {
         // const speed = isMobile ? 100 : 10;
-         const speed = /iPhone/i.test(navigator.userAgent) ? 3 : isMobile ? 10 : 50;
+        const speed = /iPhone/i.test(navigator.userAgent) ? 5 : isMobile ? 60 : 80;
 
         const scrollStep = () => {
             if (!isScrollingRef.current) return;
